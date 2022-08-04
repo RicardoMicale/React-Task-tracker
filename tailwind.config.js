18eb09b0ad4bfe8e9c88c05b9c799f828096c148
Ricardo Micale <ricardomicale@gmail.com>
2022-08-04 17:20:01 -0400

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#ffffff',
+      'gray': '#202020',
+      'light-gray': '#f4f4f4',
+      'blue': '#0096c7',
+      'green': '#2dc653',
+      'red': '#d90429'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
