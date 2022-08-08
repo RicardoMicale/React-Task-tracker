34878d19a58483bec0c39af19721018ab306c749
Ricardo Micale <ricardomicale@gmail.com>
2022-08-08 13:17:05 -0400

    dark blue added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ module.exports = {
       'white': '#ffffff',
       'gray': '#202020',
       'light-gray': '#f4f4f4',
-      'blue': '#0096c7',
+      'blue': '#016FB9',
       'green': '#2dc653',
-      'red': '#d90429'
+      'red': '#d90429',
+      'dark-blue': '#0D4C6F',
     },
     extend: {},
   },
